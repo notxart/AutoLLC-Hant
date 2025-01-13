08d9b0dafe26a88c7e65d6515412ab9b87fd9a1e
Traxton <Traxton.GPG@proton.me>
2025-01-13 22:39:57 +0800

    Fixed the wrong download URL and module installation issues. (#8)

--- limbus-hant.ps1
+++ limbus-hant.ps1
@@ -1,17 +1,19 @@
-$DEBUG_MODE = $false
+# Debug
+param([switch]$Debug = $false)
+if ($Debug) { $DebugPreference = "Continue" }
 
 # Function to get Steam installation path from registry
 function Get-SteamPath {
     try {
         $steamPath = (Get-ItemProperty -Path "HKCU:\Software\Valve\Steam").SteamPath
         if (-Not (Test-Path -Path $steamPath)) {
-            throw "未找到有效的 Steam 路徑。"
+            throw "未找到有效的 Steam 路徑，腳本已終止。"
         }
         return $steamPath
     }
     catch {
-        Write-Host $_.Exception.Message
-        exit
+        Write-Error $_.Exception.Message
+        exit 1
     }
 }
 
@@ -33,21 +35,28 @@ foreach ($folder in $libraryFolders) {
     }
 }
 if (-Not ($gamePath)) {
-    Write-Output "未找到遊戲 Limbus Company 的安裝目錄，腳本已終止。"
-    exit
+    Write-Error "未找到遊戲 Limbus Company 的安裝目錄，腳本已終止。"
+    exit 1
 }
-Write-Output "已找到遊戲 Limbus Company 的安裝目錄，繁體中文語言包將安裝於: $gamePath"
+Write-Host "已找到遊戲 Limbus Company 的安裝目錄，繁體中文語言包將安裝於: $gamePath"
 
-# Install 7Zip4Powershell module
-if (-Not (Get-Command -Module 7Zip4Powershell -errorAction SilentlyContinue)) {
-    Install-Module -Name 7Zip4Powershell -Force
+try {
+    # Check if 7Zip4Powershell module is installed
+    if (-Not (Get-Command -Module 7Zip4Powershell -ErrorAction SilentlyContinue)) {
+        # Install 7Zip4Powershell module with CurrentUser scope
+        Install-Module -Name 7Zip4Powershell -Scope CurrentUser -Force
+    }
+}
+catch {
+    Write-Error "無法安裝腳本必要模組: 7Zip4Powershell module，腳本已終止。"
+    exit 1
 }
 
 # Define GitHub API URLs and download targets
 $apiUrls = @(
-    "https://api.github.com/repos/LocalizeLimbusCompany/BepInEx_For_LLC/releases",
-    "https://api.github.com/repos/SmallYuanSY/LLC_ChineseFontAsset/releases",
-    "https://api.github.com/repos/SmallYuanSY/LocalizeLimbusCompany_TW/releases"
+    "https://api.github.com/repos/LocalizeLimbusCompany/BepInEx_For_LLC/releases/latest",
+    "https://api.github.com/repos/SmallYuanSY/LLC_ChineseFontAsset/releases/latest",
+    "https://api.github.com/repos/SmallYuanSY/LocalizeLimbusCompany_TW/releases/latest"
 )
 $targets = @(
     "https.*BepInEx-IL2CPP-x64.*.7z",
@@ -93,21 +102,21 @@ for ($i = 0; $i -lt $apiUrls.Length; $i++) {
     foreach ($asset in $json.assets) {
         if ($asset -match $target) {
             $url = $asset.browser_download_url
-            if ($DEBUG_MODE) { Write-Output "URL: $url" }
+            Write-Debug "Download URL:`t$url"
             break
         }
     }
 
     # Check if the current URL matches the one in the history file
     if ($history.ContainsKey($apiUrl) -and $history[$apiUrl] -eq $url) {
-        if ($DEBUG_MODE) { Write-Output "Match URL: $apiUrl" } else { continue }
+        if ($Debug) { Write-Debug "Match API URL:`t$apiUrl" } else { continue }
     }
 
     # Download, unzip, and remove compressed files
     $fileName = "limbus_i18n_$i.7z"
     Invoke-WebRequest $url -OutFile $fileName
     Expand-7Zip -ArchiveFileName $fileName -TargetPath $gamePath
-    if (-Not $DEBUG_MODE) { Remove-Item $fileName }
+    if (-Not $Debug) { Remove-Item $fileName }
 
     # Update the history with the new URL
     $history[$apiUrl] = $url
@@ -115,5 +124,5 @@ for ($i = 0; $i -lt $apiUrls.Length; $i++) {
 
 # Write the updated history file and start the game.
 Write-HistoryFile -record $history
-Write-Output "Limbus Company 繁體中文語言包已順利安裝，即將為您啟動遊戲。"
+Write-Host "Limbus Company 繁體中文語言包已順利安裝，即將為您啟動遊戲。"
 Start-Process -FilePath (Join-Path $gamePath "LimbusCompany.exe")
